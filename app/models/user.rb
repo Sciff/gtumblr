ce11be6fe0da74e56d512b47b501ed41bcffc72f
Alex Ivanov <walexjar@gmail.com>
2015-05-15 19:36:16 +0600

    add app

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :current_password
+
+  validates :first_name, :last_name, presence: true
+
+  mount_uploader :avatar, AvatarUploader
+
+  def password_required?
+    super && provider.blank?
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      params.permit(:current_password)
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
+
+  class << self
+    def from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+      end
+    end
+
+    def new_with_session(params, session)
+      if session['devise.user_attributes']
+        new(session['devise.user_attributes'], without_protection: true) do |user|
+          user.attributes = params
+          user.valid?
+        end
+      else
+        super
+      end
+    end
+  end
+
+end
